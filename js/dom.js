a73866c557be5330e977539831a0db2a8c5d3bd2
Tamás Komoróczy <thomas@nyirszikszi.hu>
2021-06-02 17:20:55 +0200

    first commit

---
+++ js/dom.js
@@ -0,0 +1,6 @@
+let cellak = document.getElementsByClassName('cella');
+for(let i=0; i<cellak.length; i++)
+    console.log( cellak[i].innerHTML)
+document.getElementById('r1c1').style.backgroundColor="lime";
+cellak[0].innerHTML = "X";
+    
